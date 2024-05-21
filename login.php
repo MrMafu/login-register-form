fd0ecde0bfa958c6e2548e4362a28ee1c6f44c3c
MrMafu <kinanradiaputra@gmail.com>
2024-05-21 08:38:20 +0700

    Made the field input go by column

--- login.php
+++ login.php
@@ -1,5 +1,4 @@
 <?php 
-//Mengecek apakah $_GET msg ada atau tidak, contoh $_GET = login.php?msg=Username%20atau%20Password%20salah!! , "Username%20atau%20Password%20salah!!" merupakan variabel $_GET['msg']
 $msg = (isset($_GET['msg']) ? $_GET['msg']: "");
 ?>
 <!DOCTYPE html>
@@ -13,9 +12,9 @@ $msg = (isset($_GET['msg']) ? $_GET['msg']: "");
 <body>
     <a href="index.php"><button>Home</button></a>
     <h1>Login</h1>
-    <form action="./control.php" method="post">
-        <input type="text" name="username" placeholder="Ketik nama disini..">
-        <input type="password" name="password" placeholder="Ketik password disini..">
+    <form class="field" action="./control.php" method="post">
+        <input type="text" name="username" placeholder="Username">
+        <input type="password" name="password" placeholder="Password">
         <button type="submit" name="action" value="login">Login</button>
     </form>
     <?= (!empty($msg) ? "<h2>Error: $msg</h2>" : "") ?>
--- register.php
+++ register.php
@@ -9,9 +9,9 @@
 <body>
     <a href="index.php"><button>Home</button></a>
     <h1>Register</h1>
-    <form action="./control.php" method="post">
-        <input type="text" name="username" placeholder="Ketik nama disini..">
-        <input type="password" name="password" placeholder="Ketik password disini..">
+    <form class="field" action="./control.php" method="post">
+        <input type="text" name="username" placeholder="Username">
+        <input type="password" name="password" placeholder="Password">
         <button type="submit" name="action" value="register">Register</button>
         <h4>Sudah punya akun? <a href="login.php">Login</a></h4>
     </form>
